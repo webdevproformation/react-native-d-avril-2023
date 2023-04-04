19461ddb9a83662b427203d6bd753959c37e9aa2
Malik H <malik.h@webdevpro.net>
2023-04-04 14:51:08 +0200

    premier composant

--- jour1-decouverte/App.js
+++ jour1-decouverte/App.js
@@ -1,14 +1,49 @@
+// composant racine (root) de notre projet
+// divisé en 3 parties 
+// import importer des fonctionnalités qui sont stockées dans le dossier node_modules
+// React Native utilise le système de module ES6 
+//  il n'y a ./ devant les noms des librairies => node_modules  
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+// le composant => fonction javascript 
+// App() il faut OBLIGATOIREMENT s'appeler App 
+// return (
+//  pas html  pas du javascript pas xml 
+// JSX mix entre html / js et xml 
+//)
+// HMR => Hot Module Reload react et expo 
+
+import { Bonjour } from "./composants/Bonjour"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bonjour les amis comment allez vous ??</Text>
+      <Bonjour />
+      {/* Composant => un peu comme si on crée une balise  */}
+      {/* function Bonjour(){} Bonjour() => <Bonjour /> */}
+      {/* commentaire en jsx  */}
+      <Text>Formation Doranco 2023</Text>
+      <Text>groupe CDA</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// facultative 
+// permet de mettre en forme le composant 
+// pas du css => css à la sauce javascript 
+// <Text> => html balise => React Composant 
+// <Text> => <p> <h1> <h2> <li>
+// <View> => <div>
+// <StatusBar> => zone de haut de l'écran 
+// <Text>  </Text> composant ouvert et fermant
+// <StatusBar style="auto" /> composant orphelin ne pas oublier le / dans le composant 
+// <p class="first" >coucou</p> => attribut de la balise p 
+// <View style={styles.container}> => style  attribut ou props
+
+// https://reactnative.dev/
+// https://github.com/eramudeep/react-native-ecommerce
 
 const styles = StyleSheet.create({
   container: {
---
+++ jour1-decouverte/composants/Bonjour.jsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native"
+
+export function Bonjour(){
+    return <Text>Bonjour</Text>
+}
