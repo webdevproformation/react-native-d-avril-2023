08a3c4a47039e78e0fc61b752e26e01705da7298
Malik H <malik.h@webdevpro.net>
2023-04-04 12:25:55 +0200

    import export système de module ES

---
+++ jour2-js6/05-calcul.js
@@ -0,0 +1,14 @@
+export function calcul(){
+    console.log("je suis un calcul")
+}
+
+export const a = ["tutu"]
+
+const nom = "Alain"
+
+// avec le système de module
+// la fonction calcul QUE dans le fichier 05-calcul.js => isolée 
+
+// a est utilisable dans le fichier et hors du fichier 
+
+// nom n'est utilisable QUE dans le fichier 05-calcul.js => isolée 
