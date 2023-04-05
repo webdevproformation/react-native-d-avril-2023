c95f2d431abf164d28dab1a9bbfb8a2a5cff52b2
Malik H <malik.h@webdevpro.net>
2023-04-05 17:03:50 +0200

    fin des notes du jours

--- jour3-composants/composant/Exo2.jsx
+++ jour3-composants/composant/Exo2.jsx
@@ -10,12 +10,17 @@ const Exo2 = () => {
             <Image source={{ uri : "https://source.unsplash.com/random/110x111" , width:110, height:110 }} style={styles.mb}/>
             <Image source={{ uri : "https://source.unsplash.com/random/110x112" , width:110, height:110 }} style={styles.mb}/>
             <Image source={{ uri : "https://source.unsplash.com/random/110x113" , width:110, height:110 }} style={styles.mb}/>
-            <Image source={{ uri : "https://source.unsplash.com/random/110x114" , width:110, height:110 }} style={styles.mb} fadeDuration={5000}/>
+            <Image source={{ uri : "https://source.unsplash.com/random/110x114" , width:110, height:110 }} style={styles.mb} fadeDuration={10000}/>
             <Image source={{ uri : "https://source.unsplash.com/random/110x115" , width:110, height:110 }} style={styles.mb} blurRadius={0} />
       </View>
     </View>
   )
 }
+// composant de base 
+// evenemnt => onPress => 
+// hook => fonction de React qui permet de gérer des données en local dans le composant
+// useState
+// useEffect => requête ajax  => api cocktail 
 
 export default Exo2
 
