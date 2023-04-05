3f40dbf98587ee312208870982e0fa2186040935
Malik H <malik.h@webdevpro.net>
2023-04-05 11:11:43 +0200

    correction

--- jour3-composants/App.js
+++ jour3-composants/App.js
@@ -5,11 +5,20 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.zone1} >
-        <Text style={{ fontSize : 30 }}>premier</Text>
+        <Text style={{ fontSize : 50 }}>premier</Text>
         <Text>deuxieme</Text>
         <Text>troisieme</Text>
       </View>
-      <View style={{ backgroundColor : "blue" , flex : 2 , width:"100%" }} />
+      <View style={{ backgroundColor : "blue" , flex : 2 , width:"100%" , justifyContent: "space-between" }} >
+        <View style={{ flexDirection : "row" , justifyContent: "space-between" }}>
+          <Text style={{color : "white"}}>Premier</Text>
+          <Text style={{color : "white"}}>Deuxieme</Text>
+        </View>
+        <View style={{ flexDirection : "row" , justifyContent: "space-between" }}>
+          <Text style={{color : "white"}}>Troisieme</Text>
+          <Text style={{color : "white"}}>Quatrieme</Text>
+        </View>
+      </View>
       <View style={{ backgroundColor : "yellow" , flex : 0.5 , width:"100%" }} />
       {/*<StatusBar hidden={true} />*/}
       <StatusBar style="auto" />
@@ -29,7 +38,7 @@ const styles = StyleSheet.create({
     flex : 1 , 
     width:"100%" , 
     flexDirection: "row" , 
-    justifyContent : "space-around" , /* attention justifyContent centerer horizontalement SI flexDirection: "row" par défaut  justifyContent: "center" va centrer Verticalement
+    justifyContent : "space-around" , /* attention justifyContent centre horizontalement SI flexDirection: "row" par défaut  justifyContent: "center" va centrer Verticalement
       flex-start
       flex-end
       center 
@@ -37,11 +46,15 @@ const styles = StyleSheet.create({
       space-between
       space-around
     */ 
-   alignItems: "baseline" // verticalement SI flexDirection: "row" ,  par contre si on est sur flexDirection: "column" permet de gérer l'espacement horizontalement 
+    alignItems: "baseline" // verticalement SI flexDirection: "row" ,  par contre si on est sur flexDirection: "column" permet de gérer l'espacement horizontalement 
     // flex-start
     // flex-end
     // center
     // baseline
-
+    // 10h50 bon café !!!
   }
+  // cas pratique :
+  // dans la zone bleue
+  // Ajouter dans cette zone 4 textes premier deuxieme troisieme et quatrieme
+  // caler chaque mot dans un coin de cette zone 
 });
