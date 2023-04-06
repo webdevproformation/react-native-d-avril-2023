374381879b9202942004f622d897a6605cef63f1
Malik H <malik.h@webdevpro.net>
2023-04-06 13:54:50 +0200

    correction

--- jour4-communication/App.js
+++ jour4-communication/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Premier from './composants/Premier';
+import Diapositive from './composants/Diapositive';
 
 export default function App() {
 
@@ -11,11 +12,18 @@ export default function App() {
     { largeur : 60 , hauteur : 12 , unite : "mm"},
   ]
 
+  const diapositives = [
+    "https://source.unsplash.com/random/200x100" , 
+    "https://source.unsplash.com/random/200x101" , 
+    "https://source.unsplash.com/random/200x102"
+  ];
+
   return (
     <View style={styles.container}>
-        {/* exécute le composant Premier dans App en lui ajoutant des props
+      {/* exécute le composant Premier dans App en lui ajoutant des props
         en html => attribut 
-        vous pouvez en mettre autant que nécessaire nommer comme vous voulez */}
+        vous pouvez en mettre autant que nécessaire nommer comme vous voulez 
+      */}
        {/*<Premier largeur={10} hauteur="40" unite="cm" />
        <Premier largeur={30} hauteur="30" unite="km" />
        <Premier largeur={60} hauteur="12" unite="mm" />
@@ -30,6 +38,11 @@ export default function App() {
                   {/** props spéciale qui permet à React Native de distinguer les différents éléments <Premier /> de la liste */}
       }) }
       {/*[<Premier largeur={10} hauteur="40" unite="cm" /> ,<Premier largeur={30} hauteur="30" unite="km" /> , <Premier largeur={60} hauteur="12" unite="mm" /> , <Premier largeur={60} hauteur="12" unite="mm" /> ] */}
+      <View style={{ flexDirection:"row" , justifyContent: "center" }}>
+        {diapositives.map(function(diapo , index){
+          return <Diapositive url={diapo} key={index} / >
+        })}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
---
+++ jour4-communication/composants/Diapositive.jsx
@@ -0,0 +1,6 @@
+// rnfes
+import { Image } from 'react-native'
+
+const Diapositive = props => <Image source={{ uri : props.url , width: 100 , height : 100  }} />
+
+export default Diapositive
