a606854da7ea46d8ae2e22fe94642a3bfa4e51e7
Malik H <malik.h@webdevpro.net>
2023-04-06 15:31:20 +0200

    fin correction

--- jour4-communication/composants/Like.jsx
+++ jour4-communication/composants/Like.jsx
@@ -17,3 +17,22 @@ const styles = StyleSheet.create({
     box : {flexDirection : "row" , alignItems: "flex-end" , marginBottom: 30},
     espace : {marginLeft: 10}
 })
+
+/**
+npm i react-native-svg react-native-svg-transformer
+npx expo install react-native-svg@13.4.0
+
+npx expo customize metro.config.js
+
+svg 
+
+npm i 
+
+npx create-expo-app --template => ajouter svg en + 
+
+// props => App => Composant (Premier)
+// Like state local que l'on modifier avec setNb => mettre à jour la vue 
+// Essayer d'envoyer des info° => parent => Enfant => enfant lorsque l'on va cliquer sur 
+// une action > modidifier la valeur stockée dans le parent 
+
+*/
--- jour4-communication/metro.config.js
+++ jour4-communication/metro.config.js
@@ -1,30 +1,4 @@
-/**
- * Metro configuration for React Native with svg support
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-const { getDefaultConfig } = require('metro-config');
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+module.exports = getDefaultConfig(__dirname);
