bbf83eef19e43c08573b2d02f9ff11d8da418d57
Malik H <malik.h@webdevpro.net>
2023-04-12 11:52:06 +0200

    composant Formulaire

--- jour7-context/composant/Formulaire.jsx
+++ jour7-context/composant/Formulaire.jsx
@@ -1,13 +1,28 @@
 import { StyleSheet, Text, View , TextInput, Button } from 'react-native'
-import React from 'react'
+import React , {useContext , useState} from 'react'
+import { ProfilContext } from "../contexts/profilContext"
 
 const Formulaire = () => {
+
+  const { profil, login } = useContext(ProfilContext);
+  const [email , setEmail] = useState("")
+  const [password , setPassword] = useState("")
+  function authentification(){
+    const identifiants = {
+      email : email ,
+      password : password
+    }
+    console.log(identifiants)
+    login(identifiants)
+    console.log(profil)
+  }
+
   return (
     <View style={styles.box}>
       <Text>Formulaire</Text>
-      <TextInput placeholder='email' style={styles.input} />
-      <TextInput placeholder='password' style={styles.input} />
-      <Button title="soumettre" onPress={() => {}}/>
+      <TextInput placeholder='email' style={styles.input} onChangeText={(text) => setEmail(text)} value={email}/>
+      <TextInput placeholder='password' style={styles.input} onChangeText={(text) => setPassword(text)} value={password} />
+      <Button title="soumettre" onPress={() => authentification()}/>
     </View>
   )
 }
