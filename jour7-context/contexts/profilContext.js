e44db48294d78142900eb34d2b2f4681c2b5653e
Malik H <malik.h@webdevpro.net>
2023-04-12 11:38:54 +0200

    fin du context

--- jour7-context/contexts/profilContext.js
+++ jour7-context/contexts/profilContext.js
@@ -1,7 +1,7 @@
 // vous pouvez l'appel comme vous voulez 
 // dans ce fichier que l'on va créer un state global 
 
-import { createContext } from "react" // cette fonction qui permet de créer un état global
+import { createContext , useState } from "react" // cette fonction qui permet de créer un état global
 
 export const ProfilContext = createContext() ; // store 
 
@@ -9,13 +9,34 @@ export function ProfilContextProvider (props){
     // un composant qui va emballer l'intégralité de notre application 
     // rdv 10h50 @ toute suite !!
     // Provider => Fournir à l'ensemble de notre App le state global
-    const profil = {
+    const [profil , setProfil] = useState({
         nom : "Alain" ,
         email : "a@yahoo.fr",
         password : "123456",
         isLogged : false
+    })
+
+    function login(crediantials){ // identifiants (connexion)
+        const cloneProfil = {...profil}
+        if(crediantials.email === profil.email && crediantials.password === profil.password ){
+            cloneProfil.isLogged = true 
+            setProfil(cloneProfil)
+            return ;
+        }
+        cloneProfil.message = "identifiants invalides" ; 
+        setProfil(cloneProfil)
     }
-    return <ProfilContext.Provider value={profil} >
+
+    function logout(){ // change la valeur isLogged de profil => true => false (déconnexion)
+        const cloneProfil = {...profil} ; 
+        cloneProfil.isLogged = false 
+        setProfil(cloneProfil)
+    }
+
+    // le context n'est pas PERSISTANT => modifier dans la RAM les valeurs du profils
+    // MAIS si le smartphone s'éteint ou modification code => les valeurs modifiées sont 
+    // remise à zero
+    return <ProfilContext.Provider value={{ profil , login , logout}} >
         {props.children}
         {/** children est une propriété spéciale de props 
          * permet d'afficher les composants à l'intérieur d'un autre 
