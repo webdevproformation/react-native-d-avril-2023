ad6f73234ea6a30993950ee3dd59807d48c5fbee
Malik H <malik.h@webdevpro.net>
2023-04-14 10:17:54 +0200

    création de la base et de la table

--- jour9-sqlite/App.js
+++ jour9-sqlite/App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SQLITE from "expo-sqlite"
+import {useEffect} from "react"
+
+const db = SQLITE.openDatabase("demo.sqlite");
 
 export default function App() {
+
+  useEffect( function(){
+
+    db.transaction(function(tx){
+      tx.executeSql(`CREATE TABLE IF NOT EXISTS articles (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titre VARCHAR(200),
+                    contenu TEXT ,
+                    dt_creation DATETIME DEFAULT CURRENT_TIMESTAMP
+                  )`,
+                    [],
+                    function(transact, resultat){ console.log("table article créé") },
+                    function(transact , err){ console.log("erreur lors de la création") })
+    })
+
+  } , [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titre}>Utiliser SQLITE dans React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +35,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop : 30
   },
+  titre:{
+    textAlign : "center",
+    fontSize : 20
+  }
 });
