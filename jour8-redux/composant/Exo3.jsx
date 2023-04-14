9d0bef0bdec127a4185593ea284843589e682d80
Malik H <malik.h@webdevpro.net>
2023-04-14 09:47:29 +0200

    correction

--- jour8-redux/composant/Exo3.jsx
+++ jour8-redux/composant/Exo3.jsx
@@ -1,9 +1,9 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { useDispatch } from "react-redux"
+import { useDispatch , useSelector } from "react-redux"
 
 const Exo3 = () => {
-
+    const profil = useSelector(store => store.reducerProfil)
   const dispatch = useDispatch()
 
   return (
@@ -11,6 +11,9 @@ const Exo3 = () => {
       <Text>Exo3</Text>
       <Button onPress={() => dispatch({type : "AUGMENTER_PLUS_CINQ"})} title="+5" />
       <Button onPress={() => dispatch({type : "AUGMENTER_PLUS_CINQ_BIS", payload : 5})} title="+5" />
+      <Text>{JSON.stringify(profil)}</Text>
+      <Button onPress={() => dispatch({type : "LOGIN"})} title="login" />
+      <Button onPress={() => dispatch({type : "LOGOUT"})} title="logout" />
     </View>
   )
 }
